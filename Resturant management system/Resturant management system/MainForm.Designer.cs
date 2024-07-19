13c24d15c709e3e34f7492c3aa66b469d3fe32ad
GameRiot64 <159271841+GameRiot6408@users.noreply.github.com>
2024-07-19 12:24:17 +0530

    Updated UI elements

---
+++ Resturant management system/Resturant management system/MainForm.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace Resturant_management_system
+{
+    partial class Main_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel_Nav = new System.Windows.Forms.Panel();
+            this.panel_logo = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.btn_product = new System.Windows.Forms.Button();
+            this.btn_Staff = new System.Windows.Forms.Button();
+            this.btn_Kitchen = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel_Nav.SuspendLayout();
+            this.panel_logo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel_Nav
+            // 
+            this.panel_Nav.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(105)))));
+            this.panel_Nav.Controls.Add(this.button3);
+            this.panel_Nav.Controls.Add(this.btn_product);
+            this.panel_Nav.Controls.Add(this.btn_Staff);
+            this.panel_Nav.Controls.Add(this.btn_Kitchen);
+            this.panel_Nav.Controls.Add(this.panel_logo);
+            this.panel_Nav.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel_Nav.Location = new System.Drawing.Point(0, 0);
+            this.panel_Nav.Name = "panel_Nav";
+            this.panel_Nav.Size = new System.Drawing.Size(229, 450);
+            this.panel_Nav.TabIndex = 1;
+            // 
+            // panel_logo
+            // 
+            this.panel_logo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(32)))), ((int)(((byte)(54)))));
+            this.panel_logo.Controls.Add(this.pictureBox1);
+            this.panel_logo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel_logo.Location = new System.Drawing.Point(0, 0);
+            this.panel_logo.Name = "panel_logo";
+            this.panel_logo.Size = new System.Drawing.Size(229, 91);
+            this.panel_logo.TabIndex = 0;
+            // 
+            // button3
+            // 
+            this.button3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.Gainsboro;
+            this.button3.Image = global::Resturant_management_system.Properties.Resources.table_2_removebg_preview1;
+            this.button3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button3.Location = new System.Drawing.Point(0, 316);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(229, 75);
+            this.button3.TabIndex = 4;
+            this.button3.Text = " Table Management";
+            this.button3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button3.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // btn_product
+            // 
+            this.btn_product.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btn_product.FlatAppearance.BorderSize = 0;
+            this.btn_product.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_product.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_product.ForeColor = System.Drawing.Color.Gainsboro;
+            this.btn_product.Image = global::Resturant_management_system.Properties.Resources.item_removebg_preview___Copy;
+            this.btn_product.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_product.Location = new System.Drawing.Point(0, 241);
+            this.btn_product.Name = "btn_product";
+            this.btn_product.Padding = new System.Windows.Forms.Padding(2, 0, 0, 0);
+            this.btn_product.Size = new System.Drawing.Size(229, 75);
+            this.btn_product.TabIndex = 3;
+            this.btn_product.Text = "   Inventory Management";
+            this.btn_product.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_product.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btn_product.UseVisualStyleBackColor = true;
+            // 
+            // btn_Staff
+            // 
+            this.btn_Staff.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btn_Staff.FlatAppearance.BorderSize = 0;
+            this.btn_Staff.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Staff.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Staff.ForeColor = System.Drawing.Color.Gainsboro;
+            this.btn_Staff.Image = global::Resturant_management_system.Properties.Resources.staff_removebg_preview;
+            this.btn_Staff.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_Staff.Location = new System.Drawing.Point(0, 166);
+            this.btn_Staff.Name = "btn_Staff";
+            this.btn_Staff.Size = new System.Drawing.Size(229, 75);
+            this.btn_Staff.TabIndex = 2;
+            this.btn_Staff.Text = "  Staff Management";
+            this.btn_Staff.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_Staff.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btn_Staff.UseVisualStyleBackColor = true;
+            // 
+            // btn_Kitchen
+            // 
+            this.btn_Kitchen.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btn_Kitchen.FlatAppearance.BorderSize = 0;
+            this.btn_Kitchen.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Kitchen.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Kitchen.ForeColor = System.Drawing.Color.Gainsboro;
+            this.btn_Kitchen.Image = global::Resturant_management_system.Properties.Resources.kitchen_removebg;
+            this.btn_Kitchen.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_Kitchen.Location = new System.Drawing.Point(0, 91);
+            this.btn_Kitchen.Name = "btn_Kitchen";
+            this.btn_Kitchen.Size = new System.Drawing.Size(229, 75);
+            this.btn_Kitchen.TabIndex = 1;
+            this.btn_Kitchen.Text = " Kitchen Management";
+            this.btn_Kitchen.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_Kitchen.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btn_Kitchen.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = global::Resturant_management_system.Properties.Resources.WhatsApp_Image_2024_07_19_at_11_25_43_AM_removebg_preview__3_;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(229, 91);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Main_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel_Nav);
+            this.Name = "Main_Form";
+            this.Text = "Main_Form";
+            this.panel_Nav.ResumeLayout(false);
+            this.panel_logo.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel_Nav;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btn_product;
+        private System.Windows.Forms.Button btn_Staff;
+        private System.Windows.Forms.Button btn_Kitchen;
+        private System.Windows.Forms.Panel panel_logo;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
