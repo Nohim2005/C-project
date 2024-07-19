a6c9dc0af290c4f0e0d6b8c23a14f2b53b2642f6
Chani <Chani@GAMERIOTPC>
2024-07-20 02:28:27 +0530

    Add project files.

---
+++ Resturant management system/MainForm.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Resturant_management_system
+{
+
+    public partial class Main_Form : Form
+    {
+        public Main_Form()
+        {
+            InitializeComponent();
+        }
+        #region Flowmenus/Submenu hide/show
+
+        //DO NOT MESS WITH "hideSubMenu()" and "showSubMenu" DO NOT!!!!
+        private void hideSubMenu()
+        {
+            if (panel_KitchenSub.Visible == true)
+                panel_KitchenSub.Visible = false;
+            if (panel_StaffSub.Visible == true)
+                panel_StaffSub.Visible = false;
+            if (panel_ProductSub.Visible == true)
+                panel_ProductSub.Visible = false;
+            if (panel_TableSub.Visible == true)
+                panel_TableSub.Visible = false;
+
+        }
+
+        private void showSubMenu(Panel subMenu) 
+        {
+            if (subMenu.Visible == false)
+            {
+                hideSubMenu();
+                subMenu.Visible = true;
+            }
+            else
+            {
+                subMenu.Visible = false;
+            }
+
+        }
+        #endregion
+
+        private Form activeForm = null;
+        private void openChildForm(Form childForm)
+        {
+            if (activeForm != null)
+                activeForm.Close();
+            activeForm = childForm;
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            childForm.Dock = DockStyle.Fill;
+            panel_ChildSpawn.Controls.Add(childForm);
+            panel_ChildSpawn.Tag = childForm;
+            childForm.BringToFront();
+            childForm.Show();
+            
+        }
+
+        #region Main Buttons (nothing more to do here)
+        private void btn_Kitchen_Click(object sender, EventArgs e)
+        {
+            
+            showSubMenu(panel_KitchenSub);
+
+        }
+
+        private void panel_Nav_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btn_Staff_Click(object sender, EventArgs e)
+        {
+            //hideSubMenu();
+            showSubMenu(panel_StaffSub);
+
+        }
+
+        private void btn_product_Click(object sender, EventArgs e)
+        {
+            //hideSubMenu();
+            showSubMenu(panel_ProductSub);
+        }
+
+        private void btn_Table_Click(object sender, EventArgs e)
+        {
+            //hideSubMenu();
+            showSubMenu(panel_TableSub);
+        }
+        #endregion
+
+        #region //Kitchen Sub Buttons (your code goes here)//
+        private void btn_KitchenSub_Menu_Click(object sender, EventArgs e)
+        {
+            openChildForm(new MenuOptions());
+        }
+
+        private void btn_KitchenSub_Orders_Click(object sender, EventArgs e)
+        {
+            openChildForm(new Orders());
+        }
+
+        private void btn_KitchenSub_Reports_Click(object sender, EventArgs e)
+        {
+
+        }
+        #endregion
+
+        #region //Staff Sub Buttons (your code goes here)//
+
+        private void btn_StaffSub_EmployeeProfile_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_StaffSub_Tasks_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_StaffSub_Schedule_Click(object sender, EventArgs e)
+        {
+
+        }
+        #endregion
+
+        #region //Products Sub Buttons (your code goes here)//
+
+        private void btn_ProductSub_Inventory_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_ProductSub_Suppliers_Click(object sender, EventArgs e)
+        {
+
+        }
+        #endregion
+
+        #region //Table Sub Buttons (your code goes here)//
+        private void btn_TableSub_Reservations_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_TableSub_Cleaning_Click(object sender, EventArgs e)
+        {
+
+        }
+        #endregion
+
+
+
+
+    }
+}
---
+++ Resturant management system/MenuOptions.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace Resturant_management_system
+{
+    partial class MenuOptions
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(532, 316);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Menu Options Form";
+            // 
+            // MenuOptions
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(1275, 656);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MenuOptions";
+            this.Text = "MenuOptions";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
